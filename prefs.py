bb34696c39d5072274590570cdeeb90ca4ac8cba
OStillman <owenstillman@live.co.uk>
2018-09-19 14:52:49 +0100

    Added Timezone offset stored to user's login

---
+++ prefs.py
@@ -0,0 +1,16 @@
+import webapp2
+import models
+
+class PrefsPage(webapp2.RequestHandler):
+    def post(self):
+        userprefs = models.get_userprefs()
+        try:
+            tz_offset = float(self.request.get('tz_offset'))
+            userprefs.tz_offset = tz_offset
+            userprefs.put()
+        except ValueError:
+        # User Entered value that isn't a float. Ignore it..
+            pass
+
+        self.redirect('/')
+application = webapp2.WSGIApplication([('/prefs', PrefsPage)], debug = True)
